37097118ca639d5d297d75a2f570d0fb5016a99b
ACookieAddi <aberger202@gmx.info>
2024-10-17 10:13:19 +0200

    NewTry

---
+++ src/client/java/de/luwallia/DEAClient.java
@@ -0,0 +1,10 @@
+package de.luwallia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DEAClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
